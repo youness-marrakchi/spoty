32db0e31ff2fce5ce1378665bb9e77bf83420fbb
youness-marrakchi <younessmarr13@gmail.com>
2023-06-23 15:29:27 +0100

    First Spoty commit

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { Database } from "@/types_db";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useState } from "react";
+
+interface SupabaseProvidersProps {
+    children: React.ReactNode;
+}
+
+const SupabaseProvider: React.FC<SupabaseProvidersProps> = ({children}) => {
+    const [supabaseClient] = useState(() => createClientComponentClient<Database>());
+    return(
+        <SessionContextProvider supabaseClient={supabaseClient}>
+            {children}
+        </SessionContextProvider>
+    )
+}
+
+export default SupabaseProvider;
