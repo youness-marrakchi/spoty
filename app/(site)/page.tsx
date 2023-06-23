5220e6d0c22e2c7c0108e15211848c8140bfabaa
youness-marrakchi <younessmarr13@gmail.com>
2023-06-23 16:01:04 +0100

    Fixed link path

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
         <div className="mb-2">
           <h1 className="text-white text-3xl font-semibold">Welcome Back</h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
-            <ListItem name="Liked Songs" href="Liked" image="/images/liked.png" />
+            <ListItem name="Liked Songs" href="liked" image="/images/liked.png" />
           </div>
         </div>
       </Header>
